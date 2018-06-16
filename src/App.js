9d6be240c987ba5681d346b9f6f92d623f5eb587
Kevin Chung <kevinchungw@gmail.com>
2018-06-15 22:35:49 -0700

    Open Sesame

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+function openDiv(){
+  console.log('success');
+  var hiddenDiv = document.getElementById("hidden");
+  console.log(hiddenDiv.style.display);
+  if(hiddenDiv.style.display === "flex"){
+    hiddenDiv.style.display = "none";
+  }
+  else{
+    hiddenDiv.style.display = "flex";
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,8 +21,21 @@ class App extends Component {
         <div className="row">
           Hello World!
         </div>
-        <div className="row">
-        
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <p onClick={openDiv}> open</p>
+          </div>
+          <div className="col-4">
+          Banana
+          </div>
+          <div className="col-4">
+          Cherry
+          </div>
+        </div>
+        <div className="row justify-content-start" id="hidden">
+          <div className ="col-4">
+            sesame
+          </div>
         </div>
       </div>
     );
